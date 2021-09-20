7ce60380da9b6ba4e37b2b9b9d838e50c08af5e0
Razvan Bangu <razvan.bangu@gmail.com>
2021-09-19 18:28:50 -0700

    Refactor.

--- js/photoviewer.js
+++ js/photoviewer.js
@@ -3,6 +3,23 @@ function getHtml(template) {
   return template.join('\n');
 }
 
+function runGalleria(imageKeys){
+    const galleriaData = imageKeys.map(function(key) {
+    return {
+      big: "https://do-cdn.prazli.com/" + key,
+      thumb: "https://do-cdn.prazli.com/cdn-cgi/image/quality=10/" + key,
+    };
+  });
+
+  // document.getElementById('viewer').innerHTML = html.join("\n");
+  // document.getElementsByTagName('img')[0].setAttribute('style', 'display:none;');
+
+  Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.6.1/themes/folio/galleria.folio.min.js');
+  Galleria.run('.galleria', {
+    dataSource: galleriaData
+  });
+}
+
 // Show the photos that exist in an album.
 function viewAlbum() {
   window.fetch("https://sfo3.digitaloceanspaces.com/prazli2021")
@@ -14,7 +31,6 @@ function viewAlbum() {
             const regex = new RegExp('^u/.+$');
             const key = item.textContent;
             if (regex.test(key)) {
-              // const photoUrl = "https://do-cdn.prazli.com/" + key;
               imageKeys.push(key);
             }
           }
@@ -26,19 +42,9 @@ function viewAlbum() {
             ]);
           });
 
-          const galleriaData = imageKeys.map(function(key) {
-            return {
-              big: "https://do-cdn.prazli.com/" + key,
-              thumb: "https://do-cdn.prazli.com/cdn-cgi/image/quality=10/" + key,
-            };
-          });
+          runGalleria(imageKeys);
 
           // document.getElementById('viewer').innerHTML = html.join("\n");
           // document.getElementsByTagName('img')[0].setAttribute('style', 'display:none;');
-
-          Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.6.1/themes/folio/galleria.folio.min.js');
-          Galleria.run('.galleria', {
-            dataSource: galleriaData
-          });
         });
 }
